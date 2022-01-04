b74e759d46f4f846b662ccef991b84f62025e595
Thomas Cacciatore <tcacciatore597@gmail.com>
2022-01-03 19:58:02 -0500

    Setup basic Landmark detail view with SwiftUI.

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Thomas Cacciatore on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
